0aad02bf6c582d959788e12513e785846e991489
GargKrishna24 <gargkrishna@888@gmail.com>
2025-12-20 03:26:24 +0530

    Learn how to use formatted string and input function

---
+++ Introduction/input_output.py
@@ -0,0 +1,12 @@
+print("hello") # using for displaying the output on the screen
+
+name = "krishna garg"
+age = 23
+print("My name is ",name," and my age is ",age)
+
+# formated String
+
+print(f"my name is {name} and i am {age} year old")
+
+age = int(input(" print your age : "))
+print(age)
